2dff110cee51dfa01545c75d261613b4c75c8b42
DeniseOdeth18 <89421213+DeniseOdeth18@users.noreply.github.com>
2022-01-06 16:06:49 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+
+
+
+function preload(){
+  
+  
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //añadir la imagen de fondo
+
+  
+
+//creando el spritr del jugador
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  // mover al jugador hacia arriba y hacia abajo y hacer que el juego sea compatible con dispositivos móviles usando toques
+
+
+// suelta balas y cambia la imagen del tirador a la posición de disparo cuando se presiona el espacio
+
+
+// el jugador vuelve a la imagen de pie original una vez que dejamos de presionar la barra espaciadora
+
+
+drawSprites();
+
+}
